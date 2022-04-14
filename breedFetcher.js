a557b84c1f7bd44bd79b8f6dbad485cdded400cb
Charles Ngo <charlesngo37@gmail.com>
2022-04-14 16:00:55 -0700

    Updated to separate functions from index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,19 @@
 const request = require('request');
-const breed = process.argv.slice(2)[0];
 
-request(`https://api.thecatapi.com/v1/breeeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log("Error connecting to API. Please check the URL of the API. ");
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.log("Error has occured. Status code :", response.statusCode);
-    return;
-  }
-  let parsedBody = JSON.parse(body)[0];
-  if (parsedBody === undefined) {
-    console.log("Please enter in a valid cat breed. ");
-    return;
-  }
-  console.log(parsedBody.description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback("Error connecting to API. Please check the URL of the API. ");
+    }
+    if (response.statusCode !== 200) {
+      return callback(`URL is not correct. Status code :${response.statusCode}`);
+    }
+    let parsedBody = JSON.parse(body)[0];
+    if (parsedBody === undefined) {
+      return callback("Please enter in a valid cat breed. ");
+    }
+    callback(error, parsedBody.description);
+  });
+};
 
+module.exports = { fetchBreedDescription };
